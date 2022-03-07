f51b119ff824c44c23ac902c9b26510a744855a3
harshana-accelr <harshana.miyuranga@accelr.site>
2022-03-08 00:53:24 +0530

    3rd commit-final_v2

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from lib.DocIterator import DocIterator
 from lib.SectionParser import SectionParser
 import json
 
-text_line = " 1. (1) This Act may be cited as the Coronavirus Disease 2019 (COVID -19) (Temporary Provisions) " \
-                "Act, No.17 of 2021. The provisions of this Act shall be in operation for a period of two " \
-                "years commencing from March 1, 2020."
-
 
 def do_work():
-    doc_iterator = DocIterator("1.pdf")
+    doc_iterator = DocIterator("11.pdf")
     doc_parser = SectionParser(doc_iterator, "body")
     json_object = json.dumps(doc_parser.get_dict(), indent=4)
 
